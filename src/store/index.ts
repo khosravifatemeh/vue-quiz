1ca6a99e986d8d1f41f4b8f0d181ed5fb3adb608
Fatemeh Khosravi <uni.khosravi@gmail.com>
2020-10-07 14:53:08 +0330

    feat: add related object to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,35 @@
-import { createStore } from "vuex";
+import {
+  createStore,
+  Store as VuexStore,
+  CommitOptions,
+  DispatchOptions,
+  createLogger
+} from "vuex";
+import { State, state } from "./state";
+import { Mutations, mutations } from "./mutations";
+import { Actions, actions } from "./actions";
 
-export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+export const store = createStore({
+  state,
+  mutations,
+  actions,
+  plugins: [createLogger()]
 });
+
+export type Store = Omit<VuexStore<State>, "commit" | "dispatch"> & {
+  commit<K extends keyof Mutations, P extends Parameters<Mutations[K]>[1]>(
+    key: K,
+    payload: P,
+    options?: CommitOptions
+  ): ReturnType<Mutations[K]>;
+} & {
+  dispatch<K extends keyof Actions>(
+    key: K,
+    payload: Parameters<Actions[K]>[1],
+    options?: DispatchOptions
+  ): ReturnType<Actions[K]>;
+};
+
+export function useStore() {
+  return store as Store;
+}
